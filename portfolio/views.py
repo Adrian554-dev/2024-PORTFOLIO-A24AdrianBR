642b6df44c8a445837b400f0cf8e5d2b4e230280
A24AdrianBR <a24adrianbr@iessanclemente.net>
2024-11-15 12:51:40 +0100

    clase

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from .models import Project
 
 def home(request):
-    # return HttpResponse("Hellow there friend")
     projects=Project.objects.all()
     return render(request, 'generator/home.html',{'projects':projects})
 
